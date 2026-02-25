b61eb651706fbb87ca06f74ae1d8b79255a98264
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-02-25 10:49:03 +0100

    Button Component Created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import {NavbarComponent} from './layouts/navbar/navbar';
 import {HomePage} from './pages/home-page/home-page';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
     NavbarComponent,
-    HomePage
+    RouterOutlet
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'

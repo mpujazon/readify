37610376da31c118ab8a40d3b0e26deecb373968
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-02-25 11:31:13 +0100

    Feature: Footer Component Implemented

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,14 @@ import { Component, signal } from '@angular/core';
 import {NavbarComponent} from './layouts/navbar/navbar';
 import {HomePage} from './pages/home-page/home-page';
 import {RouterOutlet} from '@angular/router';
+import {Footer} from './layouts/footer/footer';
 
 @Component({
   selector: 'app-root',
   imports: [
     NavbarComponent,
-    RouterOutlet
+    RouterOutlet,
+    Footer
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'

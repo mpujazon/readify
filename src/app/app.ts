19921dfc561253721056a6ed860ac00c916e310e
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-02-25 10:46:47 +0100

    Feat: HomePage Component Done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import {NavbarComponent} from './layouts/navbar/navbar';
+import {HomePage} from './pages/home-page/home-page';
 
 @Component({
   selector: 'app-root',
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    HomePage
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'

93aac44fa0eaf41ce59fdd1710c24a6077d190c0
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-03-05 08:13:09 +0100

    Refactor: Deleted link property on Button Component

--- src/app/components/atoms/button/button.ts
+++ src/app/components/atoms/button/button.ts
@@ -1,15 +1,13 @@
 import { Component, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { ButtonStyle } from '../../../types/Button';
 
 @Component({
   selector: 'app-button',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './button.html'
 })
 export class Button {
   label = input.required<string>();
   buttonStyle = input.required<ButtonStyle>();
-  link = input<string>();
   icon = input<string>();
 }

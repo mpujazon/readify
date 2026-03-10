f3bbc48b52f0e330e8b84654d29e87f9c5907823
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-03-10 09:52:48 +0100

    Refactor: getBook Function now is consuming the api mock using observables. book-details.resolver fixed

--- src/app/pages/book-details/book-details.resolver.ts
+++ src/app/pages/book-details/book-details.resolver.ts
@@ -1,5 +1,6 @@
 import { inject } from '@angular/core';
 import { RedirectCommand, ResolveFn, Router } from '@angular/router';
+import { map } from 'rxjs';
 import { BooksService } from '../../services/books-service';
 import { Book } from '../../types/Book';
 
@@ -8,7 +9,9 @@ export const bookResolver: ResolveFn<Book> = (route) => {
   const booksService = inject(BooksService);
 
   try {
-    return booksService.getBook(route.paramMap.get('id') ?? '');
+    return booksService.getBook(route.paramMap.get('id') ?? '').pipe(
+      map((book) => book ?? new RedirectCommand(router.parseUrl('/404')))
+    );
   } catch {
     return new RedirectCommand(router.parseUrl('/404'));
   }
--- src/app/services/books-service.ts
+++ src/app/services/books-service.ts
@@ -1,8 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {Book} from '../types/Book';
-import {books} from '../data/books';
+import { map, Observable } from 'rxjs';
+import { Book } from '../types/Book';
 
 @Injectable({
   providedIn: 'root',
@@ -11,15 +10,14 @@ export class BooksService {
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:3000/books';
 
-  getAllBooks(): Observable<Book[]>{
+  getAllBooks(): Observable<Book[]> {
     return this.http.get<Book[]>(this.apiUrl);
   }
-  getBook(id: string){
-    if(!id) throw new Error('Need to provide a book id.')
+  getBook(id: string): Observable<Book | undefined> {
+    if (!id) throw new Error('Need to provide a book id.');
 
-    const book = books.find(book=> book.id === id);
-    if(!book) throw new Error(`There is no books with  id ${id}`);
-
-    return book;
+    return this.getAllBooks().pipe(
+      map((books) => books.find((book) => book.id === id))
+    );
   }
 }
